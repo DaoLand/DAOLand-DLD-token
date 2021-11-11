bcb3aa46358d5a4eed9833bbc4e5dd31c699f875
user.name=gbortnikov <g_bortnikov@list.ru>
2021-11-11 13:50:12 +0700

    IAntisnipe

--- contracts/TokenDLD.sol
+++ contracts/TokenDLD.sol
@@ -5,41 +5,31 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract TokenDLD is ERC20, AccessControl {
+interface IAntisnipe {
+    function assureCanTransfer(
+        address account,
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+contract TokeDLD is ERC20, AccessControl {
     using SafeERC20 for IERC20;
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    uint256 public constant initialSupply =  100_000_000*1e18; // 100M tokens(with 18 decimals)
     address private owner;
+    uint256 public constant initialSupply = 100_000_000 * 1e18; // 1_000_000 tokens(with 18 decimals)
 
-    mapping(address => bool) public whitelist;
-    mapping(address => bool) public blocklist;
-
-    bool public paused;
-
-    modifier notBlocked(address _recipient) {
-        require(
-            !blocklist[msg.sender] && !blocklist[_recipient],
-            "You are in blocklist"
-        );
-        _;
-    }
-
-    modifier pausable(address _recipient) {
-        if (paused) {
-            require(
-                whitelist[msg.sender] || whitelist[_recipient],
-                "Only whitelisted users can transfer while token paused!"
-            );
-        }
-        _;
-    }
+    bool public antisnipeEnabled = true;
+    IAntisnipe public antisnipe =
+        IAntisnipe(0x2E5dDfb5F950fd98fb159E1FA9ABc8DB12DCcFCf);
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(ADMIN_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        _setRoleAdmin(ADMIN_ROLE, DEFAULT_ADMIN_ROLE);
 
         _mint(msg.sender, initialSupply);
 
@@ -63,28 +53,17 @@ contract TokenDLD is ERC20, AccessControl {
     }
 
     function _beforeTokenTransfer(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) internal override notBlocked(_to) pausable(_to) {
-        super._beforeTokenTransfer(_from, _to, _amount);
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        if (antisnipeEnabled && address(antisnipe) != address(0)) {
+            require(antisnipe.assureCanTransfer(msg.sender, from, to, amount));
+        }
     }
 
-    function setPause(bool _state) public onlyRole(ADMIN_ROLE) {
-        paused = _state;
-    }
-
-    function setWhiteStatus(address _user, bool _state)
-        public
-        onlyRole(ADMIN_ROLE)
-    {
-        whitelist[_user] = _state;
-    }
-
-    function setBlockStatus(address _user, bool _state)
-        public
-        onlyRole(ADMIN_ROLE)
-    {
-        blocklist[_user] = _state;
+    function setAntisnipeDisable() external onlyRole(ADMIN_ROLE) {
+        require(antisnipeEnabled);
+        antisnipeEnabled = false;
     }
 }
